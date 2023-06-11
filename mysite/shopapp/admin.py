df1e56f249d7f0b81f1d1aea78d4e9e10e5d7902
Светлана Скобельцына <skobsvetlana@gmail.com>
2023-06-11 14:01:36 +0300

    product, order in admin added commit

--- mysite/shopapp/admin.py
+++ mysite/shopapp/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Order
+
+class OrderInline(admin.TabularInline):
+    model = Product.orders.through
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderInline,
+    ]
+    #list_display = "pk", "name", "description", "quantity", "price", "discount", "created_at", "archived"
+    list_display = "pk", "name", "description_short", "quantity", "price", "discount", "created_at", "archived"
+    list_display_links = "pk", "name"
+    ordering = "-name", "pk"
+    search_fields = "name", "description", "price"
+
+    def description_short(self, obj: Product) -> str:
+        if len(obj.description) <= 50:
+            return obj.description
+        else:
+            return obj.description[:50] + "..."
+
+#admin.site.register(Product, ProductAdmin)
+
+
+# class ProductInline(admin.TabularInline):
+class ProductInline(admin.StackedInline):
+    model = Order.products.through
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductInline,
+    ]
+    list_display = "delivery_address", "promocode", "created_at", "user_verbose"
+
+    def get_queryset(self, request):
+        return Order.objects.select_related("user").prefetch_related("products")
+
+    def user_verbose(self, obj: Order) -> str:
+        return obj.user.first_name or obj.user.username
--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -16,6 +16,16 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     archived = models.BooleanField(default=False)
 
+    # @property
+    # def description_short(self) -> str:
+    #     if len(self.description) <= 50:
+    #         return self.description
+    #     else:
+    #         return self.description[:50] + "..."
+
+    def __str__(self) -> str:
+        return f"Product (pk={self.pk}, name={self.name!r})"
+
 
 class Order(models.Model):
     delivery_address = models.TextField(null=False, blank=True)
