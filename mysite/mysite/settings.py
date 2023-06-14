f45ac616d3b70bd50a20aa48dad05f9c95f664d1
Светлана Скобельцына <skobsvetlana@gmail.com>
2023-06-14 16:36:15 +0300

    requestdataapp created  commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shopapp.apps.ShopappConfig',
+    'requestdataapp.apps.RequestdataappConfig',
 ]
 
 MIDDLEWARE = [
