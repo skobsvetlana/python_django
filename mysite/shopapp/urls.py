2dde8a2e2eb1173d52c7e846dbd8571a2de507bd
Светлана Скобельцына <skobsvetlana@gmail.com>
2023-06-21 11:15:19 +0300

    produt details created commit

--- mysite/shopapp/urls.py
+++ mysite/shopapp/urls.py
@@ -1,14 +1,22 @@
 from django.urls import path
-from shopapp.views import shop_index, groups_list, products_list, \
-    orders_list, create_product, create_order
+from shopapp.views import (
+    ShopIndexView,
+    GroupsListView,
+    products_list,
+    orders_list,
+    create_product,
+    create_order,
+    ProductDetailsView,
+)
 
 app_name = "shopapp"
 
 urlpatterns = [
-    path("", shop_index, name="index"),
-    path("groups/", groups_list, name="groups_list"),
+    path("", ShopIndexView.as_view(), name="index"),
+    path("groups/", GroupsListView.as_view(), name="groups_list"),
     path("products/", products_list, name="products_list"),
     path("products/create/", create_product, name="create_product"),
+    path("products/<int:pk>/", ProductDetailsView.as_view(), name="product_details"),
     path("orders/", orders_list, name="orders_list"),
     path("orders/create/", create_order, name="create_order"),
 ]
--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -3,42 +3,64 @@ from timeit import default_timer
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.models import Group
+from django.views import View
 
-from shopapp.forms import ProductForm, OrderForm
+from shopapp.forms import ProductForm, OrderForm, GroupForm
 from shopapp.models import Product, Order
 
-def shop_index(request: HttpRequest):
-    products = [
-        ('Laptop', 1999),
-        ('Desktop', 2999),
-        ('Smartphone', 999),
-    ]
-    cities = [
-        {'name': 'Mumbai', 'population': '19,000,000', 'country': 'India'},
-        {'name': 'Calcutta', 'population': '15,000,000', 'country': 'India'},
-        {'name': 'New York', 'population': '20,000,000', 'country': 'USA'},
-        {'name': 'Chicago', 'population': '7,000,000', 'country': 'USA'},
-        {'name': 'Tokyo', 'population': '33,000,000', 'country': 'Japan'},
-    ]
-    greeting_str = "hello shop index!"
-    num_messages = 6
-    context = {
-        "time_running": default_timer(),
-        "products": products,
-        "cities": cities,
-        "greeting_str": greeting_str,
-        "num_messages": num_messages,
-    }
+class ShopIndexView(View):
+    def get(self, request: HttpRequest) ->HttpResponse:
+        products = [
+            ('Laptop', 1999),
+            ('Desktop', 2999),
+            ('Smartphone', 999),
+        ]
+        cities = [
+            {'name': 'Mumbai', 'population': '19,000,000', 'country': 'India'},
+            {'name': 'Calcutta', 'population': '15,000,000', 'country': 'India'},
+            {'name': 'New York', 'population': '20,000,000', 'country': 'USA'},
+            {'name': 'Chicago', 'population': '7,000,000', 'country': 'USA'},
+            {'name': 'Tokyo', 'population': '33,000,000', 'country': 'Japan'},
+        ]
+        greeting_str = "hello shop index!"
+        num_messages = 6
+        context = {
+            "time_running": default_timer(),
+            "products": products,
+            "cities": cities,
+            "greeting_str": greeting_str,
+            "num_messages": num_messages,
+        }
 
-    return render(request, 'shopapp/shop_index.html', context=context)
+        return render(request, 'shopapp/shop_index.html', context=context)
 
 
-def groups_list(request: HttpRequest):
-    context = {
-        "groups": Group.objects.prefetch_related('permissions').all(),
-    }
+class GroupsListView(View):
+    def get(self, request: HttpRequest) ->HttpResponse:
+        context = {
+            "form": GroupForm(),
+            "groups": Group.objects.prefetch_related('permissions').all(),
+        }
 
-    return render(request, 'shopapp/groups-list.html', context=context)
+        return render(request, 'shopapp/groups-list.html', context=context)
+
+    def post(self, request: HttpRequest):
+        form = GroupForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect(request.path)
+        #return self.get(request)
+
+
+class ProductDetailsView(View):
+    def get(self, request: HttpRequest, pk: int) -> HttpResponse:
+        product = Product.objects.get(pk=pk)
+        context = {
+            "product": product
+        }
+
+        return render(request, 'shopapp/product-details.html', context=context)
 
 
 def products_list(request: HttpRequest):
