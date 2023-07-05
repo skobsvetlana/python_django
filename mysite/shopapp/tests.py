90ca8fc5c4bdc85282e6e9455d993b475f91ddd1
Светлана Скобельцына <skobsvetlana@gmail.com>
2023-07-05 13:16:37 +0300

    task10 initial commit

--- mysite/shopapp/tests.py
+++ mysite/shopapp/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from shopapp.utils import add_two_numbers
 
-# Create your tests here.
+class AddTwoNumbers(TestCase):
+    def test_add_two_numbers(self):
+        result = add_two_numbers(2, 3)
+        self.assertEqual(result, 5)
+
