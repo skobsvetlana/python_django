4ebf9e3c9cfcf6667361c2a46df276e937312487
Светлана Скобельцына <skobsvetlana@gmail.com>
2023-09-03 16:23:47 +0300

    locale commit

--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -13,8 +13,8 @@ def product_preview_directory_path(instance: "Product", filename: str) -> str:
 class Product(models.Model):
     class Meta:
         ordering = ["name", "price"]
-        #verbose_name = _("Product")
-        #verbose_name_plural = "products"
+        verbose_name = _("Product")
+        verbose_name_plural = "products"
         #db_table = "tech_products"
 
     name = models.CharField(max_length=100)
