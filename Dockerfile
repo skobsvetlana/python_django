84754e2a9c1f145aceac541382e13ef0e45a4fad
Светлана Скобельцына <skobsvetlana@gmail.com>
2023-11-16 17:26:03 +0300

    task20 configure for deployment commit

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,4 @@ RUN pip install -r requirements.txt
 
 COPY mysite .
 
-#CMD ["python", "manage.py", "runserver"]
+CMD ["gunicorn", "mysite.wsgi:application", "--bind", "0.0.0.0.8000"]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -47,12 +47,14 @@ SECRET_KEY = getenv(
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = getenv("DJANGO_DEBUG", "0") == "1"
 
 ALLOWED_HOSTS = [
     "0.0.0.0",
     '127.0.0.1',
-]
+] + getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
+
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
